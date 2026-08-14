7ff444431634497ac054307f6f101769201aebc2
zjmorgan <zjmorgan@mtu.edu>
2026-08-14 13:32:00 -0400

    Updates

--- src/garnet/utilities/calibration.py
+++ src/garnet/utilities/calibration.py
@@ -285,7 +285,7 @@ class Calibration:
 
         mtd["peaks"].sample().getOrientedLattice().setU(np.eye(3))
 
-        # IndexPeaks(PeaksWorkspace="peaks", Tolerance=0.2)
+        IndexPeaks(PeaksWorkspace="peaks", Tolerance=0.2)
 
         mtd["peaks"].run().getGoniometer().setR(np.eye(3))
 
@@ -836,7 +836,7 @@ class Calibration:
                 label="{}_panel".format(iteration),
                 show_calibrated=True,
             )
-            if self.refine_goniometer:
+            if self.refine_off:
                 self.calibrate_goniometer(iteration)
         self.generate_diagnostic(self.iterations)
 
